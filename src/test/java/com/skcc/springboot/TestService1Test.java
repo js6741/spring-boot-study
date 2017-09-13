071ac6ab7070799e4b08cffee596294071db997c
HyunSub Shim <rexshim@gmail.com>
2017-09-13 09:31:09 +0100

    TestService1 test

---
+++ src/test/java/com/skcc/springboot/TestService1Test.java
@@ -0,0 +1,27 @@
+package com.skcc.springboot;
+
+import com.skcc.springboot.test.TestService1;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("s1")
+public class TestService1Test {
+
+    @Autowired
+    private TestService1 testService1;
+
+	@Test
+	public void contextLoads() {
+	    assertThat(testService1.getString(), is("some string"));
+	}
+
+}
